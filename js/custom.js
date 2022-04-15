733e815b7264288655564e7c193ffa4c8e604289
Westley-Winks <Westley-Winks@users.noreply.github.com>
2022-04-15 21:34:42 +0000

    deploy: 9ffaeda4cc4c2b24c84d3a8964fd73b062ee8c86

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,13 @@
 // Insert custom javascript here.
+function show() {
+
+    /* Access image by id and change
+    the display property to block*/
+    var contentId = document.getElementById("hidden-image");
+
+    if (contentId.style.display === "none") {
+        contentId.style.display = "block";
+      } else {
+        contentId.style.display = "none";
+      }
+}
